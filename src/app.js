d647ac99b15a4284fa333ef151b8bd23645fc1ef
David F14 <davidfalue@gmail.com>
2023-04-19 00:11:41 +0200

    express vue

--- src/app.js
+++ src/app.js
@@ -1,10 +1,25 @@
 const express = require('express');
+const path = require('path');
 const app = express();
+const axios = require('axios');
 
+// Définir le répertoire statique pour les fichiers Vue.js
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Route principale pour servir le fichier HTML de l'application Vue.js
 app.get('/', (req, res) => {
-  res.send('Bonjour, bienvenue sur mon site Web!');
+  axios.get('https://valorant-api.com/v1/agents')
+  .then(response => {
+    console.log(response.data);
+  })
+  .catch(error => {
+    console.error(error);
+  });
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+// ... Autres routes et configurations Express
+// Démarrer le serveur Express
 app.listen(3001, () => {
-  console.log('Le serveur est en écoute sur le port 3001!');
+  console.log('Serveur Express en cours d\'exécution sur le port 3001');
 });
